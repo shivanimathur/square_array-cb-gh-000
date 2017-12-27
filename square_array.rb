acf2e88ff68c081a0e739d54854d6ef1a3bcbc60
Shivani Mathur <qwellery@gmail.com>
2017-12-27 12:38:32 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   # your code here
-  square_array = array
   array.each do |element|
-    square_array = element * element
+    element *= 2
   end
-  return square_array
+  
 end
